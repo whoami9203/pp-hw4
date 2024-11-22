fd28157657510cba0589ef849f9a6ec935be2776
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2024-11-23 00:48:13 +0800

    no check flag

--- sample/hw4.cu
+++ sample/hw4.cu
@@ -46,11 +46,11 @@ typedef struct _block
     unsigned int nonce;
 }HashBlock;
 
-typedef struct _sharedData
+typedef struct __align__(32) _sharedData
 {
     HashBlock block;
-    SHA256 sha256_ctx;
     SHA256 tmp;
+    SHA256 sha256_ctx;
 }SharedData;
 
 __constant__ static const WORD k[64] = {
@@ -468,37 +468,34 @@ void h_double_sha256(SHA256 *sha256_ctx, unsigned char *bytes, size_t len)
 ////////////////////   Find Nonce   /////////////////////
 
 
-__global__ void find_nonce(__restrict__ HashBlock *block, unsigned char* __restrict__ target, unsigned int *solution, unsigned char *found_flag) {
+__global__ void find_nonce(__restrict__ HashBlock *block, unsigned char* __restrict__ target, unsigned int *solution) {
     __shared__ SharedData d_data[BLOCK_SIZE];
 
     d_data[threadIdx.x].block = *block;
     d_data[threadIdx.x].block.nonce = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (*found_flag == 0) {
-        // Compute double SHA-256
-        double_sha256(&d_data[threadIdx.x].tmp, &d_data[threadIdx.x].sha256_ctx, (unsigned char*)&d_data[threadIdx.x]);
+    // Compute double SHA-256
+    double_sha256(&d_data[threadIdx.x].tmp, &d_data[threadIdx.x].sha256_ctx, (unsigned char*)&d_data[threadIdx.x]);
 
-        const unsigned int *a_int = reinterpret_cast<const unsigned int*>(d_data[threadIdx.x].sha256_ctx.b);
-        const unsigned int *b_int = reinterpret_cast<const unsigned int*>(target);
-        // compared from lowest bit
-        int result = 0;
-        result = (result << 1) + (a_int[7] > b_int[7]) - (a_int[7] < b_int[7]);
-        result = (result << 1) + (a_int[6] > b_int[6]) - (a_int[6] < b_int[6]);
-        result = (result << 1) + (a_int[5] > b_int[5]) - (a_int[5] < b_int[5]);
-        result = (result << 1) + (a_int[4] > b_int[4]) - (a_int[4] < b_int[4]);
-        result = (result << 1) + (a_int[3] > b_int[3]) - (a_int[3] < b_int[3]);
-        result = (result << 1) + (a_int[2] > b_int[2]) - (a_int[2] < b_int[2]);
-        result = (result << 1) + (a_int[1] > b_int[1]) - (a_int[1] < b_int[1]);
-        result = (result << 1) + (a_int[0] > b_int[0]) - (a_int[0] < b_int[0]);
+    const unsigned int *a_int = reinterpret_cast<const unsigned int*>(d_data[threadIdx.x].sha256_ctx.b);
+    const unsigned int *b_int = reinterpret_cast<const unsigned int*>(target);
+    // compared from lowest bit
+    int result = 0;
+    result = (result << 1) + (a_int[7] > b_int[7]) - (a_int[7] < b_int[7]);
+    result = (result << 1) + (a_int[6] > b_int[6]) - (a_int[6] < b_int[6]);
+    result = (result << 1) + (a_int[5] > b_int[5]) - (a_int[5] < b_int[5]);
+    result = (result << 1) + (a_int[4] > b_int[4]) - (a_int[4] < b_int[4]);
+    result = (result << 1) + (a_int[3] > b_int[3]) - (a_int[3] < b_int[3]);
+    result = (result << 1) + (a_int[2] > b_int[2]) - (a_int[2] < b_int[2]);
+    result = (result << 1) + (a_int[1] > b_int[1]) - (a_int[1] < b_int[1]);
+    result = (result << 1) + (a_int[0] > b_int[0]) - (a_int[0] < b_int[0]);
 
-        // Check if the hash is less than the target
-        if (result < 0) {
-            // Write the solution and signal that a valid nonce is found
-            *solution = d_data[threadIdx.x].block.nonce;
-            *found_flag = 1;
-            return; // Exit the kernel
-        }
+    // Check if the hash is less than the target
+    if (result < 0) {
+        // Write the solution and signal that a valid nonce is found
+        *solution = d_data[threadIdx.x].block.nonce;
     }
+    
 }
 
 ////////////////////   Merkle Root   /////////////////////
@@ -675,13 +672,13 @@ void solve(FILE *fin, FILE *fout)
     }
 
     // Set the cache configuration for the kernel
-    cudaFuncSetCacheConfig(find_nonce, cudaFuncCachePreferShared);
+    // cudaFuncSetCacheConfig(find_nonce, cudaFuncCachePreferShared);
 
     auto start_kernel = std::chrono::high_resolution_clock::now();
     // Launch kernel
     int threads_per_block = 256;
     int blocks_per_grid = 16777216; // Adjust based on your GPU
-    find_nonce<<<blocks_per_grid, threads_per_block>>>(d_block, d_target, d_solution, d_found_flag);
+    find_nonce<<<blocks_per_grid, threads_per_block>>>(d_block, d_target, d_solution);
 
     err = cudaGetLastError();
     if (err != cudaSuccess){
